1b67c7105aac1a764e6de09f2c1298a0bbb79a19
ChrisPollini <pollinikristofbarnabas@gmailc.om>
2019-10-10 19:38:02 +0200

    User class and UserBuilder database is implemented

---
+++ src/database/UserBuilder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+
+public class UserBuilder extends PlayerBuilder {
+
+    private String email;
+    private String password;
+
+    public UserBuilder password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder email(String email) {
+        this.email = email;
+        return this;
+    }
+}
