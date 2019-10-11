ae7855d83acae80854d9077fb978043f7270456e
ChrisPollini <pollinikristofbarnabas@gmailc.om>
2019-10-11 09:27:12 +0200

    Main is modified

--- src/sportbetting/Main.java
+++ src/sportbetting/Main.java
@@ -1,8 +1,16 @@
 package sportbetting;
 
+import database.Database;
+import exception.TerminateAppExcpetion;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import service.SportBettingSerivce;
+import view.View;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        App app = new App(new SportBettingSerivce(new Database()), new View());
+        app.play();
     }
 }
